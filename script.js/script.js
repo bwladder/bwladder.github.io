80a6cc539c3ecbbc95e14d91ec8c3ac50c9388d1
gingerhatesayan <ayan9322khan@gmail.com>
2025-04-15 04:22:01 +0530

    initial commit

---
+++ script.js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for reaching out to LADDER. We'll get back to you shortly!");
+});
